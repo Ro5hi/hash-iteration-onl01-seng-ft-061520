493fe9a6437cc10b1da81adb17faacecd6322737
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-19 20:10:27 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,6 +8,7 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   
+  end 
 end 
 
 def age_appropriate_birthday(birthday_kids)
@@ -16,5 +17,5 @@ def age_appropriate_birthday(birthday_kids)
      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
      else
        puts "You are to old for this."
- end
+  end
 end
