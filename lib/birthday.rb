51112d00c0e8da79a0e58da9840ddb86abd6014f
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-19 20:10:10 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,6 +16,5 @@ def age_appropriate_birthday(birthday_kids)
      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
      else
        puts "You are to old for this."
-   end
  end
 end
